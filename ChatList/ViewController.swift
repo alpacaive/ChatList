7dc51aff77dcdbafe3deb8d4a97c5ae4f1269a38
alpacaive <w6352800@naver.com>
2026-03-08 21:15:38 +0900

    Initial Commit

---
+++ ChatList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatList
+//
+//  Created by 이머영 on 3/8/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
