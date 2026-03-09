736837939a25f784d962c7f1d12e319ae01552f1
alpacaive <w6352800@naver.com>
2026-03-09 23:55:20 +0900

    viewController 에 Data, Presentation, Layout 주입

--- ChatList/ChatListViewController.swift
+++ ChatList/ChatListViewController.swift
@@ -8,22 +8,44 @@
 import UIKit
 
 class ChatListViewController: UIViewController {
-
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let chatList: [Chat] = Chat.list
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Data, Presentation, Layout
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension ChatListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return chatList.count
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ChatListCollectionViewCell", for: indexPath) as? ChatListCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        let chat = chatList[indexPath.item]
+        cell.configure(chat)
+        
+        return cell
+    }
+    
+}
 
+extension ChatListViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: collectionView.bounds.width, height: 100)
+    }
 }
